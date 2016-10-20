0fea0d40bd9a17a095b83e5c66fbf3e8e5f3a05a
gaspard <gaspard@gmail.com>
2016-10-20 12:02:02 +0200

    use combineReducers

--- src/store.js
+++ src/store.js
@@ -1,11 +1,12 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import todos from './todos.js';
 import visibilityFilter from './visibilityFilter.js';
 
-const todoApp = (state = {}, action) => ({
-  todos: todos(state.todos, action),
-  visibilityFilter: visibilityFilter(state.visibilityFilter, action)
+const todoApp = combineReducers({
+  todos,
+  visibilityFilter
 });
+
 const store = createStore(todoApp);
 
 export default store;
---
+++ src/store.test.js
@@ -0,0 +1,14 @@
+import store from './store';
+
+it('tests store', () => {
+
+  store.dispatch({type:'SET_VISIBILITY_FILTER', filter:'SHOW_COMPLETED'})
+  expect(
+    store.getState().visibilityFilter
+  ).toEqual('SHOW_COMPLETED');
+
+  store.dispatch({type:'TODO_ADD', text: 'Add a todo', id: Math.floor(Math.random()*100000)});
+  expect(
+    store.getState().todos[0].text
+  ).toEqual('Add a todo');
+})
