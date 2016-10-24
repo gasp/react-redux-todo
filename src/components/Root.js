814e5dd0374e262dca1d85f70a35364dcc786e7d
gaspard <gaspard@gmail.com>
2016-10-24 17:05:53 +0200

    route: first route in root okay, this looks confusing

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,12 +1,18 @@
-import React from 'react';
-
+import React, { PropTypes } from 'react';
 import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router'
 import TodoApp from '../components/TodoApp';
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <TodoApp />
+    <Router history={browserHistory}>
+      <Route path='/' component={TodoApp} />
+    </Router>
   </Provider>
 );
+
+Root.propTypes = {
+  store: PropTypes.object.isRequired
+}
 
 export default Root;
